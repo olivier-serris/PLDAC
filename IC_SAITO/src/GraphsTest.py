21d7ad734b44ccd0eb0df4854d47399e8fce1590
Olivier <olivier.serris@wanadoo.fr>
2020-04-13 19:35:57 +0200

    Eval and save network  and Algo

--- IC_SAITO/src/GraphsTest.py
+++ IC_SAITO/src/GraphsTest.py
@@ -10,11 +10,10 @@ import pandas as pd
 import pprint as pp
 from networkx import nx
 import GraphGen as GGen
+import matplotlib.pyplot as plt
 
+DATA_PATH = '../data/eval/'
 
-
-def CrossVal_MAP(model,D):
-    return Metrics.MAP(model.graph,D)
 
 def handmadeGraphTest():
     handmadeGraph=dok_matrix((5,5),dtype=np.float32)
@@ -37,55 +36,90 @@ def handmadeGraphTest():
     pp.pprint(scores)
     
 
-def ScoresGraphModels(graphs,graphs_titles,models):
-    scores = dict()
-    for g,t in zip(graphs,graphs_titles) : 
-        cascades = csc.genCascades(g,40)
-        D = [csc.CascadeToTimeRepr(c) for c in cascades]
-        scores[t] = dict()
-        for model in models: 
-            print("for :",t,str(model))
-            model.set_params(csc.nodes_in_D(D))
-            #'''
-            cross_res = cross_validate(model,D,scoring=CrossVal_MAP)
-            scores[t][str(model)] = cross_res['test_score'].mean()
-            print(cross_res)
-            '''
-            model.fit(D)
-            print(str(model),"fitted")
-            scores[t][str(model)] = Metrics.MAP(model.graph,D)
-            print(str(model),f"score:{scores[t][str(model)]}")
-            '''
-        scores[t]["original"] = Metrics.MAP(g,D)
+def saveCscadesDistrib(Cascades,t):
+    lens = np.array([len(c) for c in Cascades])
+    hist = np.histogram(lens)[0]
+    plt.title(t)
+    plt.bar(np.arange(len(hist)),hist)
+    plt.savefig(DATA_PATH+t+'_csc')
+    plt.show()
+
+
+def CrossVal_MAP(model,D):
+    return Metrics.MAP(model.graph,D)
+
+def ScoresGraphModels(graphs,graphs_titles,models,nbCascades):
+    mesures = ['MSE','MAP','time']
+    data = {m:{str(g_t):dict() for g_t in graphs_titles} for m in mesures}
     
-    return pd.DataFrame(scores)
+    for g,t in zip(graphs,graphs_titles) : 
+        cascades = csc.genCascades(g,nbCascades)
+        saveCscadesDistrib(cascades,t)
+        D = [csc.CascadeToTimeRepr(c) for c in cascades]
+        crossVal_MSE = lambda m,d : Metrics.MSE(g, m.graph)
+        
+        for model in models: 
+            print("\nfor :",t,str(model))
+            model.set_params(csc.nodes_in_D(D))
+            cross_res = cross_validate(model,D,scoring={"MAP":CrossVal_MAP,"MSE":crossVal_MSE})
+            print(cross_res)
+            
+            data["MSE"][t][str(model)] = cross_res['test_MSE'].mean()
+            data["MAP"][t][str(model)] = cross_res['test_MAP'].mean()
+            data["time"][t][str(model)] = cross_res['fit_time'].mean()
 
+        data["MAP"][t]["original"] = Metrics.MAP(g,D)
 
+    dfs = [pd.DataFrame(data[m]) for m in mesures]
+    for df,m in zip(dfs,mesures):
+        df.name = m
+    return dfs
+
+def launch_test(nbCascades):
+    # Generate GRAPHS 
+    nx_to_dok_rand = lambda g : GGen.randomize_edges_values(
+                                    dok_matrix(nx.to_scipy_sparse_matrix(g)))
+    
+    scale_free = nx.scale_free_graph(100)
+    scale_free_dok = nx_to_dok_rand(scale_free)
+    
+    erdos_renyi = nx.erdos_renyi_graph(30,p=0.05)
+    erdos_renyi_dok= nx_to_dok_rand(erdos_renyi)
+    
+    connected_cave_man = nx.connected_caveman_graph(8,5)
+    connected_cave_man_dok= nx_to_dok_rand(connected_cave_man)
+    
+    bara = nx.barabasi_albert_graph(30,2)
+    bara_dok = nx_to_dok_rand(bara)
+
+    graphs =[scale_free,erdos_renyi,connected_cave_man,bara]
+    graphs_dok = [scale_free_dok,erdos_renyi_dok,connected_cave_man_dok,bara_dok]
+    graphs_titles = [f"scale_free_{scale_free_dok.shape}",
+                     f"erdos_renyi{erdos_renyi_dok.shape}",
+                     f"connected_cave_man({connected_cave_man_dok.shape})",
+                     f"barabasi {bara_dok.shape}"]
+    
+    # train and test 
+    models=  [IC_EM_NotContiguous(),IC_EM_Saito2008()]
+    dfs = ScoresGraphModels(graphs_dok,graphs_titles,models,nbCascades)
+    pd.set_option('display.max_columns', len(graphs))
+    
+    # Save graphs 
+    for g,t in zip(graphs,graphs_titles) : 
+        plt.title(t) 
+        nx.draw_networkx(g,with_labels=False,node_size=40)
+        plt.savefig(DATA_PATH+t+'_g')
+        plt.show()
+        
+    # save perfs : 
+    for df in dfs:
+        print(f"\n{df.name}:\n{df}")
+        with open(DATA_PATH+df.name,"w") as f:
+            f.write(df.to_markdown())
+            
 def main():
     print("start")
-    #handmadeGraphTest()
-    
-    scale_free = dok_matrix(nx.to_scipy_sparse_matrix(nx.scale_free_graph(100)))
-    scale_free = GGen.randomize_edges_values(scale_free)
-    sparseGraph = dok_matrix(scipy.sparse.random(30,30,density=0.05))
-    
-    connected_cave_man = dok_matrix(nx.to_scipy_sparse_matrix(nx.connected_caveman_graph(10,5)))
-    connected_cave_man= GGen.randomize_edges_values(connected_cave_man)
-    
-    bara = dok_matrix(nx.to_scipy_sparse_matrix(nx.barabasi_albert_graph(50,2,seed=1)))
-    bara = GGen.randomize_edges_values(bara)
-
-    
-    graphs = [scale_free,sparseGraph,connected_cave_man,bara]
-    graphs_titles = [f"scale_free_{scale_free.shape}",
-                     f"sparseG_{sparseGraph.shape}",
-                     f"connected_cave_man",
-                     f"barabasi {bara.shape}"]
-    
-    models=  [IC_EM_NotContiguous(),IC_EM_Saito2008()]
-    df = ScoresGraphModels(graphs,graphs_titles,models)
-    pd.set_option('display.max_columns', len(graphs))
-    print(df)
+    launch_test(100)
     print("end")
     
 
